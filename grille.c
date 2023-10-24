cb490da3e44a69315744ed7bfcaee87f77066eb9
Olivier YAMMINE <148480516+PythonDuDesert@users.noreply.github.com>
2023-10-24 21:17:11 +0200

    Update grille.c

--- grille.c
+++ grille.c
@@ -12,43 +12,43 @@ void Affiche_grille(int tab_grille[TAILLE_LIGNES][TAILLE_COLONNES]) {
         for (int c=0; c<TAILLE_COLONNES; c++) {
             switch (tab_grille[l][c]) { //On teste chaque case du tableau
                 case 0: {
-                    printf("%c", 0); //0x0
+                    printf("%c ", 0); //0x0
                     break;
                 }
                 case 1: {
-                    printf("%c", 6); //0x6
+                    printf("%c ", 6); //0x6
                     break;
                 }
                 case 2: {
-                    printf("%c", 22); //1x6
+                    printf("%c ", 22); //1x6
                     break;
                 }
                 case 3: {
-                    printf("%c", 5); //0x5
+                    printf("%c ", 5); //0x5
                     break;
                 }
                 case 4: {
-                    printf("%c", 15); //0xF
+                    printf("%c ", 15); //0xF
                     break;
                 }
                 case 5: {
-                    printf("%c", 10); //0xA
+                    printf("%c ", 10); //0xA
                     break;
                 }
                case 6: {
-                    printf("%c", 26); //1xA
+                    printf("%c ", 26); //1xA
                     break;
                }
                 case 7: {
-                    printf("%c", 1); //0x1
+                    printf("%c ", 1); //0x1
                     break;
                 }
                 case 8: {
-                    printf("%c", 11); //0xB
+                    printf("%c ", 11); //0xB
                     break;
                 }
                 case 9: {
-                    printf("%c", 14); //0xE
+                    printf("%c ", 14); //0xE
                     break;
                 }
             }
