2c2917c75af95587a4368e3b36a9557643e26201
Olivier YAMMINE <148480516+PythonDuDesert@users.noreply.github.com>
2023-10-24 20:51:57 +0200

    Create grille.h

---
+++ grille.h
@@ -0,0 +1,13 @@
+//
+// Created by OLIVIER on 22/10/2023.
+//
+
+#ifndef PROJET_GRILLE_H
+#define PROJET_GRILLE_H
+
+#endif //PROJET_GRILLE_H
+
+#define TAILLE_LIGNES 10
+#define TAILLE_COLONNES 20
+void Creation_grille(int tab_grille[TAILLE_LIGNES][TAILLE_COLONNES]);
+void Affiche_grille(int tab_grille[TAILLE_LIGNES][TAILLE_COLONNES]);
