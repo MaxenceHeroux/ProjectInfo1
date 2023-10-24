28ce1648c8e897e2d50dc8ff123f9da104a2b490
Olivier YAMMINE <148480516+PythonDuDesert@users.noreply.github.com>
2023-10-24 20:54:01 +0200

    save.h

---
+++ save.h
@@ -0,0 +1,12 @@
+//
+// Created by OLIVIER on 22/10/2023.
+//
+
+#ifndef PROJET_SAVE_H
+#define PROJET_SAVE_H
+
+#endif //PROJET_SAVE_H
+
+#define TAILLE_LIGNES 10
+#define TAILLE_COLONNES 20
+short int Sauvegarde(int tab_grille[TAILLE_LIGNES][TAILLE_COLONNES]);
