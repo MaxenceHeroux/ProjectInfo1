c3678d9e0fcd959ebbf56e77f0003a7b5a415bbb
H-Maxence <148444087+MaxenceHeroux@users.noreply.github.com>
2023-10-25 17:17:20 +0200

    timer.h

---
+++ timer.h
@@ -0,0 +1,8 @@
+//
+// Created by Maxence on 25/10/2023.
+//
+void timer (short int tab_grille[TAILLE_LIGNES][TAILLE_COLONNES]);
+#ifndef SNOOPY_TIMER_H
+#define SNOOPY_TIMER_H
+
+#endif //SNOOPY_TIMER_H
