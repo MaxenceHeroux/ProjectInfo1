5366face43af54f4dd7497becba0f24c0db46afa
julienblr <148475955+julienblr@users.noreply.github.com>
2023-10-27 13:32:29 +0200

    menu.c

--- menu.c
+++ menu.c
@@ -1,3 +1,29 @@
+#include <stdio.h>
+#include "save.h"
+#include <stdlib.h>
+#include "grille.h"
+int regle(){
+    printf("Snoopy ne peut pas se deplacer en diagonale. Il ne peut se deplacer que dans les 4 directions classiques\n"
+           "(Haut, Bas, Gauche et Droite) et d'une seule case a la fois. Evidemment, en cas d'obstacle, Snoopy ne\n"
+           "pourra pas effectuer son deplacement. De plus, chaque niveau dispose d'un timer de 120 secondes, ainsi, lorsque vous arrivez a 0 seconde vous perdez une vie. Pour gagner la partie, il faut recuperer les 4 oiseaux du niveau. . ");
+}
+int nouveaujeu(){
+    short int grille[TAILLE_LIGNES][TAILLE_COLONNES] = {0}; //Initialisation d'un tableau 2D (matrice) de 10 par 20, composée de 0.
+    //Type "short int" pour n'occupper due 2 octets mémoire au lieu de 4 octets avec un type "int".
+    Creation_grille(grille); //On crée la grille de jeu en modifiant en conséquence le tableau "grille"
+    Affiche_grille(grille);//On affiche le tableau "grille"
+
+
+}
+int charger(){
+    int short Sauvegarde(short int tab_grille[TAILLE_LIGNES][TAILLE_COLONNES]);
+}
+int mdp(){
+    int motDePasse;
+    printf("Vous pouvez acceder au niveau de votre choix si vous connaissez son mot de passe. Veuillez saisir ce dernier : ");
+    scanf("%d",&motDePasse);
+    //suivant le mdp choisi et sil correspond a un des niveaux appel de la fonction associée
+
 }
 int scores(){ //faire quand niveaux prets
 
